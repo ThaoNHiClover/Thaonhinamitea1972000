f2b9c73898184aae7435d90c93951eff76ab8bce
ThaoNHiClover <dungnhi271197@gmail.com>
2026-02-01 15:22:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err))
+const Product = require("./models/Product")
+
+Product.find()
+
 
 const express = require("express")
 const cors = require("cors")
